d8eb09bd4faa44607bcaf13217ce15ed3e67374a
Wolex <wolexprag@gmail.com>
2026-04-25 20:28:55 +0300

    feat(FSM): add Fsm, FsmState

---
+++ Assets/Systems/FSM/FsmState.cs
@@ -0,0 +1,11 @@
+﻿public abstract class FsmState
+{
+    protected readonly Fsm _fsm;
+    public FsmState(Fsm fsm)
+    {
+        _fsm = fsm;
+    }
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void Update() { }
+}
