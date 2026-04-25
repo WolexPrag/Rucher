fb83ccfe6299b7ed489b9e0e5f587de66f5a7af0
Wolex <wolexprag@gmail.com>
2026-04-25 21:05:37 +0300

    feat(GameSceneInstaller): add class LifeTimeScope

---
+++ Assets/Scenes/GameScene/GameSceneInstaller.cs
@@ -0,0 +1,35 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+public class GameSceneInstaller : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+
+        builder.Register<Fsm>(Lifetime.Singleton);
+
+        builder.Register<FsmStateSelection>(Lifetime.Singleton);
+        builder.Register<FsmStateBattle>(Lifetime.Singleton);
+        builder.Register<FsmStateRunner>(Lifetime.Singleton);
+
+        builder.RegisterComponentInHierarchy<Bootstrap>();
+        builder.RegisterComponentInHierarchy<SelectionView>();
+        builder.RegisterComponentInHierarchy<BattleView>();
+        builder.RegisterComponentInHierarchy<RunnerView>();
+
+        builder.Register<NameGenerator>(Lifetime.Singleton);
+        builder.RegisterComponentInHierarchy<CandidateGenerator>();
+
+        builder.RegisterBuildCallback(resolver =>
+        {
+            var fsm = resolver.Resolve<Fsm>();
+
+            fsm.AddState(resolver.Resolve<FsmStateSelection>());
+            fsm.AddState(resolver.Resolve<FsmStateBattle>());
+            fsm.AddState(resolver.Resolve<FsmStateRunner>());
+        });
+
+
+
+    }
+}
